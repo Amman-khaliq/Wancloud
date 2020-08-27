61149e9e7c0073349e5a81576c1b05b3ce61d8c1
Amman-khaliq <61386034+Amman-khaliq@users.noreply.github.com>
2020-08-27 15:08:24 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask,request,jsonify, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
-
+import models
 
 #Setting up the connection
 app = Flask(__name__)
@@ -19,69 +18,34 @@ ma = Marshmallow(app)
 #Used for signup/login
 auth = HTTPBasicAuth()
 
+#to check whether someone is logged in or not
+login_check = False
 
-#User Class for signing up of new user
-class User(db.Model):
-    __tablename__ = 'users'
-    #id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), primary_key=True, unique=True, index=True)
-    password_hash = db.Column(db.String(120))
-
-    def hash_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-
-
-#Item Class for lost and found items
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    location_item = db.Column(db.String(100))
-    description = db.Column(db.String(100))
-    datee = db.Column(db.String(50))
-
-    def __init__(self, name,loc, description,date):
-        self.name = name
-        self.location_item = loc
-        self.description = description
-        self.datee = date
 
 
 #This creates tables in DB if they are already not created
 db.create_all()
 
 
-#Schemas to help us when we make HTTP request
-class ItemSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'location_item', 'description','datee')
 
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('username', 'password_hash')
-
-
-item_schema = ItemSchema()
-items_schema = ItemSchema(many=True)
-user_schema = UserSchema()
+item_schema = models.ItemSchema()
+items_schema = models.ItemSchema(many=True)
+user_schema = models.UserSchema()
 
 
 #This signs up a new user
 @app.route('/signup', methods=['POST'])
 def new_user():
     username = request.json.get('username')
+    name = request.json.get('name')
     password = request.json.get('password_hash')
-    print('hhhhh',password)
+    number = request.json.get('number')
     if username is None or password is None:
         abort(400)    # missing arguments
-    if User.query.filter_by(username=username).first() is not None:
+    if models.User.query.filter_by(username=username).first() is not None:
         abort(400)
         # existing user
-    user = User(username=username)
+    user = models.User(username, name, password, number)
     user.hash_password(password)
     db.session.add(user)
     db.session.commit()
@@ -94,21 +58,23 @@ def new_user():
 def login_user():
     username = request.json.get('username')
     password = request.json.get('password_hash')
-    userr = User.query.get(username)
+    userr = models.User.query.get(username)
     if userr.verify_password(password):
+        login_check = True
         return("logged in")
     else:
-        return("Wrong password")
+        return("Try Again with correct email and password")
 
 
 #This adds a new item into the lost and found
+
 @app.route('/items',methods = ['POST'])
 def create_item():
     name = request.json['name']
     location = request.json['location_item']
     description = request.json['description']
     date = request.json['datee']
-    nItem = Item(name,location,description,date)
+    nItem = models.Item(name,location,description,date)
     db.session.add(nItem)
     db.session.commit()
     return items_schema.jsonify(nItem)
@@ -117,20 +83,20 @@ def create_item():
 #To get all items in the lost and found
 @app.route('/items',methods = ['GET'])
 def get_item():
-    allitems = Item.query.all()
+    allitems = models.Item.query.all()
     result = items_schema.dump(allitems)
     return jsonify(result)
 
 #Get specific item
 @app.route('/items/<id>',methods = ['GET'])
 def get_item1(id):
-    item = Item.query.get(id)
+    item = models.Item.query.get(id)
     return item_schema.jsonify(item)
 
 #to update an item
 @app.route('/items/<id>',methods = ['PUT'])
 def update_item(id):
-    item = Item.query.get(id)
+    item = models.Item.query.get(id)
     name = request.json['name']
     location = request.json['location_item']
     description = request.json['description']
@@ -146,11 +112,11 @@ def update_item(id):
 #To delete an item
 @app.route('/items/<id>',methods = ['DELETE'])
 def delete_item(id):
-    item = Item.query.get(id)
+    item = models.Item.query.get(id)
     db.session.delete(item)
     db.session.commit()
     return item_schema.jsonify(item)
 
-
 if __name__ == "__main__":
     app.run(debug = True)
+
